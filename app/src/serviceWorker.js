d89316582bc832f301e8decbfe7101256b097d0c
karosowka13 <sowa@student.agh.edu.pl>
2020-07-04 11:48:35 +0200

    service worker work

--- app/src/serviceWorker.js
+++ app/src/serviceWorker.js
@@ -15,41 +15,67 @@ if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		if (isLocalhost) {
 			// loading custom service worker from local relative path to serviceWorker.js from localhost
-			navigator.serviceWorker.register("./custom-sw.js");
+			navigator.serviceWorker
+				.register("./custom-sw.js")
+				.then(async function (registration) {
+					const subscription = await registration.pushManager.getSubscription();
+					if (subscription) {
+						return subscription;
+					}
+					const vapidPublicKey = process.env.REACT_APP_PUBLIC_VAPID_KEY;
+					const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+					return registration.pushManager.subscribe({
+						userVisibleOnly: true,
+						applicationServerKey: convertedVapidKey,
+					});
+				})
+				.then(function (subscription) {
+					fetch(
+						`${process.env.REACT_APP_SERVER}/auth/${localStorage.getItem(
+							"userId"
+						)}/subscribeTSS`,
+						{
+							method: "POST",
+							body: JSON.stringify(subscription),
+							headers: {
+								"Content-Type": "application/json",
+							},
+						}
+					);
+				})
+				.catch((err) => console.error("Push subscription error: ", err));
 		} else {
-			navigator.serviceWorker.register("/custom-sw.js");
+			navigator.serviceWorker
+				.register("/custom-sw.js")
+				.then(async function (registration) {
+					const subscription = await registration.pushManager.getSubscription();
+					if (subscription) {
+						return subscription;
+					}
+					const vapidPublicKey = process.env.REACT_APP_PUBLIC_VAPID_KEY;
+					const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+					return registration.pushManager.subscribe({
+						userVisibleOnly: true,
+						applicationServerKey: convertedVapidKey,
+					});
+				})
+				.then(function (subscription) {
+					fetch(
+						`${process.env.REACT_APP_SERVER}/auth/${localStorage.getItem(
+							"userId"
+						)}/subscribeTSS`,
+						{
+							method: "POST",
+							body: JSON.stringify(subscription),
+							headers: {
+								"Content-Type": "application/json",
+							},
+						}
+					);
+				})
+				.catch((err) => console.error("Push subscription error: ", err));
 		}
 	});
-	setTimeout(function () {
-		navigator.serviceWorker.ready
-			.then(async function (registration) {
-				const subscription = await registration.pushManager.getSubscription();
-				if (subscription) {
-					return subscription;
-				}
-				const vapidPublicKey = process.env.REACT_APP_PUBLIC_VAPID_KEY;
-				const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
-				return registration.pushManager.subscribe({
-					userVisibleOnly: true,
-					applicationServerKey: convertedVapidKey,
-				});
-			})
-			.then(function (subscription) {
-				fetch(
-					`${process.env.REACT_APP_SERVER}/auth/${localStorage.getItem(
-						"userId"
-					)}/subscribeTSS`,
-					{
-						method: "POST",
-						body: JSON.stringify(subscription),
-						headers: {
-							"Content-Type": "application/json",
-						},
-					}
-				);
-			})
-			.catch((err) => console.error("Push subscription error: ", err));
-	}, 60000); //1min
 }
 
 function urlBase64ToUint8Array(base64String) {
